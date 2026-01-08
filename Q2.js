d3388e8d3768fabcef652e98ff0496ac566c8f39
Sadia1609 <sadia.rahman160920@gmail.com>
2026-01-08 15:46:01 +0600

    solve task 2

---
+++ Q2.js
@@ -0,0 +1,14 @@
+//Problem 2: Count Vowels in a String
+
+function countVowel(string) {
+  vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let count = 0;
+  for (const vowel of string) {
+    if (vowels.includes(vowel)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowel("programming"));
