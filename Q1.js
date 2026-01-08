f173406e766c560f6d49cf64df9105e311849408
Sadia1609 <sadia.rahman160920@gmail.com>
2026-01-08 15:29:42 +0600

    solve task 1

--- Q1.js
+++ Q1.js
@@ -1,1 +1,10 @@
 //Problem 1: Reverse a String
+function reverseString(strings) {
+  let reverse = "";
+  for (const letter of strings) {
+    reverse = letter + reverse;
+  }
+  return reverse;
+}
+
+console.log(reverseString("hello"));
